38bf944cf2e97e902f7522b8b466d89cf8ba103b
EdsCodingAdventure <garifulineduard3@gmail.com>
2025-08-26 13:56:04 +0200

    Draw Player in the Middle of the screen

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
+    #---For capping the game at 60 FPS
     clock = pygame.time.Clock()
     dt = 0 #delta time
+    #---
 
-    #Game Loop
+    #---Game Loop: To keep the game open
     i = 1
     while i == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+
 
         #limiting the game to 60 FPS
         dt = clock.tick(60) / 1000
-
+    #---
     
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
